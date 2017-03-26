22dcbf83999b1680ca62f88572f0f5c5dffaaca9
Charlie <cmoog@fwparker.org>
2017-03-26 09:13:11 -0500

    //

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,3 +5,15 @@
 # Use BeautifulSoup and urllib to grab the text contents of the tweets
 # located on the twitter page you chose.  The .text attribute will supply the content of a soup object.
 # Have fun.  Again, nothing explicit. (15pts)
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = "https://twitter.com/neiltyson?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"
+
+page = urllib.request.urlopen(url)
+soup = BeautifulSoup(page.read(),"html.parser")
+
+print(soup.prettify())
+data_list = [x.text.strip() for x in soup.findAll("p",{"class" : "js-tweet-text"})]
+
+print(data_list)
