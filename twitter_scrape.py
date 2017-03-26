2a4982decc7a82308f54e234bee97d0b09147bd5
Charlie <cmoog@fwparker.org>
2017-03-26 09:15:11 -0500

    //

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -13,7 +13,9 @@ url = "https://twitter.com/neiltyson?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctw
 page = urllib.request.urlopen(url)
 soup = BeautifulSoup(page.read(),"html.parser")
 
-print(soup.prettify())
+#print(soup.prettify())
 data_list = [x.text.strip() for x in soup.findAll("p",{"class" : "js-tweet-text"})]
 
-print(data_list)
+for k in range(len(data_list)):
+    print(data_list[k])
+    print()
